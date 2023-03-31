671d669828eecc6506c3dcc84f83b30213ec8fb0
ryan0028 <ryan0028@student.monash.edu>
2023-03-31 15:12:42 +1100

    update project folder

---
+++ Chapter2/Function/ourfunc.cpp
@@ -0,0 +1,19 @@
+// Created by ryanw on 2023/3/31.
+#include <iostream>
+using namespace std;
+
+void simon(int); // function prototyupe
+
+//int main() {
+//    simon(3);
+//    int count;
+//    cin >> count;
+//    simon(count);
+//
+//    return 0;
+//}
+
+void simon(int n)
+{
+    cout << "Simon says touch your toes " << n << " times." << endl;
+}
