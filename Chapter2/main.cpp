525dc746fb010019fe4e33554a049a44a2ecf436
ryan0028 <ryan0028@student.monash.edu>
2023-03-31 20:52:30 +1100

    README update

--- Chapter2/main.cpp
+++ Chapter2/main.cpp
@@ -1,6 +1,21 @@
-//#include <iostream>
-//
-//int main() {
-//    std::cout << "Hello, World!" << std::endl;
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+double units(double);
+
+int main() {
+    double lightyear;
+    double unit;
+    cout << "Enter the number of light years: ";
+    cin >> lightyear;
+    unit = units(lightyear);
+    cout << lightyear << " light years = " << unit << " astronomical units";
+
+    return 0;
+}
+
+double units(double lightyear)
+{
+    double unit = lightyear * 63240;
+    return unit;
+}
