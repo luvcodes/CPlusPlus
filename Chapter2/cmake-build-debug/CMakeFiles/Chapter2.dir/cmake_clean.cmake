a8371bb8b422821d9c8f6a2c9134ed0bf91f8314
ryan0028 <ryan0028@student.monash.edu>
2023-03-31 20:55:57 +1100

    README update again

---
+++ Chapter2/cmake-build-debug/CMakeFiles/Chapter2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter2.dir/Function/convert.cpp.obj"
+  "CMakeFiles/Chapter2.dir/Function/convert.cpp.obj.d"
+  "CMakeFiles/Chapter2.dir/Function/ourfunc.cpp.obj"
+  "CMakeFiles/Chapter2.dir/Function/ourfunc.cpp.obj.d"
+  "CMakeFiles/Chapter2.dir/main.cpp.obj"
+  "CMakeFiles/Chapter2.dir/main.cpp.obj.d"
+  "Chapter2.exe"
+  "Chapter2.exe.manifest"
+  "Chapter2.pdb"
+  "libChapter2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
