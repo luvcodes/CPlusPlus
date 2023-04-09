714606b5971cff09ba40a0ea00c04bd283b8464f
Runze Yang <ryan0028@student.monash.edu>
2023-04-09 11:35:13 +1000

    For loops for Chap 5, README update

---
+++ Chapter5/ForLoop/forloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i; // create a counter
+    // initialize; test; update
+    for (i = 0; i < 5; i++) {
+        cout << "C++ knows loops" << endl;
+    }
+    cout << "C++ knows when to stop" << endl;
+
+    return 0;
+}
---
+++ Chapter5/ForLoop/num_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter the starting countdown value: ";
+    int limit;
+    cin >> limit;
+    int i;
+    for (i = limit; i; i--) { // quits when i is 0
+        cout << "i = " << i << "\n";
+    }
+    cout << "Done now that i = " << i << "\n";
+
+    return 0;
+}
