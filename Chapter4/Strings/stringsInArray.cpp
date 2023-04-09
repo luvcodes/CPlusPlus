fb5f444d27190da0d8c4567868869d07d897b854
Runze Yang <ryan0028@student.monash.edu>
2023-04-09 10:54:12 +1000

    StringsInArray update

--- Chapter4/Strings/stringsInArray.cpp
+++ Chapter4/Strings/stringsInArray.cpp
@@ -23,3 +23,25 @@ int main()
 
     return 0;
 }
+
+// int main()
+// {
+//     const int Size = 15;
+//     char name1[Size];
+//     char name2[Size] = "C++owboy";
+    
+//     cout << "Howdy! I'm " << name2;
+//     cout << "\nWhat's your name? ";
+//     cin.getline(name1, Size);
+    
+//     cout << "Well, " << name1 << ", your name has " << strlen(name1) << " characters.\n";
+//     cout << "In an array of " << sizeof (name1) << endl;
+    
+//     cout << "Your initial is " << name1[0] << endl;
+    
+//     name2[3] = '\0';
+//     cout << "First 3 characters of my name: " << name2 << endl;
+    
+    
+//     return 0;
+// }
