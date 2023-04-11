5000aa24b41b4632e51d7b39955110e5ff69f585
Runze Yang <ryan0028@student.monash.edu>
2023-04-11 11:29:04 +1000

    水仙花数联系 dowhile循环

---
+++ Chapter5/DoWhileLoops/Narcissistic_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 打印所有的三位数字
+    int num = 100;
+    do {
+        // 从所有三位数字中找到水仙花数
+        int a = 0; // 个位
+        int b = 0; // 十位
+        int c = 0; // 百位
+
+        a = num % 10; // 获取数字的个位
+        b = num / 10 % 10; // 获取数字的十位
+        c = num / 100; // 获取数字的百位
+
+        if (a*a*a + b*b*b + c*c*c == num) // 如果是水仙花数，才打印
+        {
+            cout << num << endl;
+        }
+        num++;
+    } while (num < 1000);
+
+
+}
