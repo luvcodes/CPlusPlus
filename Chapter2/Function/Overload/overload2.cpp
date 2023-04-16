b661c1f413c842d60b230b53d0eeb2cdbf60246c
ryan0028 <ryan0028@student.monash.edu>
2023-04-16 17:20:23 +1000

    Overload

---
+++ Chapter2/Function/Overload/overload2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+// 函数重载的注意事项
+// 1. 引用作为重载的条件
+void func(int& a) // int & a = 10; // 不合法
+{
+	cout << "func() calling" << endl;
+}
+
+void func(const int& a) // const int &a = 10; // 合法
+{
+	cout << "func(const int & a) calling" << endl;
+}
+
+// 2. 函数重载碰到默认参数
+void func2(int a, int b = 10)
+{
+	cout << "func2(int a, int b) calling" << endl;
+}
+
+void func2(int a)
+{
+	cout << "func2(int a) calling" << endl;
+}
+
+int main()
+{
+	//int a = 10;
+	//func(a);
+	//func(10); // 所以会调用第二个func
+	
+	//func2(10); // 当函数重载碰到默认参数，出现二义性，报错
+
+	return 0;
+}
