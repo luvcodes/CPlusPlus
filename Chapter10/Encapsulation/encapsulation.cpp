5cc7f5afd3182e6981c7413fe5d35f6350105989
ryan0028 <ryan0028@student.monash.edu>
2023-04-16 17:54:24 +1000

    system pause for exe file

--- Chapter10/Encapsulation/encapsulation.cpp
+++ Chapter10/Encapsulation/encapsulation.cpp
@@ -4,14 +4,14 @@ using namespace std;
 // 设计一个圆类，求圆的周长
 // 公式: 2*pi*r
 
+const double PI = 3.14;
+
 class Circle
 {
 	// 访问权限
-	public;
-	
+public:	
 	// 属性
 	int m_r;
-
 
 	// 行为
 	double calculateZC()
@@ -25,9 +25,9 @@ int main()
 	// 通过圆类 创建具体的圆(对象)
 	Circle c1;
 	c1.m_r = 10;
+	cout << "The perimiter is " << c1.calculateZC() << endl;
 
-	cout << "The perimiter is " << calculateZC() << endl;
-
+	system("pause");
 
 	return 0;
 }
