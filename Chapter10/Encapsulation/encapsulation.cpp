eb608a1080623b80b1433670496390c258476460
ryan0028 <ryan0028@student.monash.edu>
2023-04-16 17:37:20 +1000

    Encapsulation for OOP

---
+++ Chapter10/Encapsulation/encapsulation.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+// 设计一个圆类，求圆的周长
+// 公式: 2*pi*r
+
+class Circle
+{
+	// 访问权限
+	public;
+	
+	// 属性
+	int m_r;
+
+
+	// 行为
+	double calculateZC()
+	{
+		return 2 * PI * m_r;
+	}
+};
+
+int main()
+{
+	// 通过圆类 创建具体的圆(对象)
+	Circle c1;
+	c1.m_r = 10;
+
+	cout << "The perimiter is " << calculateZC() << endl;
+
+
+	return 0;
+}
